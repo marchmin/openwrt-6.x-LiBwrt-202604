2041c942102f1bc0655b867121d4cf96949b01ba
marchmin <462887806@qq.com>
2026-05-21 07:28:12 +0800

    Update ipq50xx.mk

--- target/linux/qualcommax/image/ipq50xx.mk
+++ target/linux/qualcommax/image/ipq50xx.mk
@@ -44,7 +44,7 @@ define Device/cmcc_mr3000d-04
 	SOC := ipq5018
 	BLOCKSIZE := 128k
 	PAGESIZE := 2048
-	IMAGE_SIZE := 59392k
+	//IMAGE_SIZE := 59392k
 	NAND_SIZE := 128m
 	DEVICE_PACKAGES := ath11k-firmware-ipq5018-qcn6122 \
 		ipq-wifi-cmcc_mr3000d-04
